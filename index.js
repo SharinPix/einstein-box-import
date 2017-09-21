3c60bf548718be94d05f4161676bf7e009a38799
kherinwork <kherin.work@gmail.com>
2017-09-21 09:44:18 +0400

    async parallel image upload

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 const Sharinpix = require('sharinpix');
-// const secretUrl = 'sharinpix://55a77236-7167-455f-a146-ce2282906880:_EiLHGQM30s_TDPBr58cqA_cOM04yjfFitDnbB0tFLLyroU@kherin.ngrok.io/api/v1';
 Sharinpix.configure(process.env.SHARINPIX_URL);
-// Sharinpix.configure(secretUrl);
-
 const sharinpixInstance = Sharinpix.get_instance();
-
 const express = require('express');
 const app = express();
 
@@ -27,11 +23,10 @@ const date = new Date();
 
 app.set('json spaces', 3);
 
-// For Content-Type: application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({
     extended: false
 }));
-// For Content-Type: application/json
+
 app.use(bodyParser.json());
 app.use(fileUpload());
 app.use(morgan(':remote-addr | :method :url :status :res[content-length] bytes - :response-time ms'));
@@ -56,7 +51,6 @@ app.post('/webhook', function (req, res) {
     let imageHeight = payload.height;
     let albumId = payload.album.public_id;
     let abilities = {};
-    var startTime, endTime;
     abilities[albumId] = {
         Access: {
             einstein_box: true
@@ -77,7 +71,6 @@ app.post('/webhook', function (req, res) {
                     let percentageHeight = (box.height / imageHeight) * 100;
                     let percentageX = (box.x / imageWidth) * 100;
                     let percentageY = (box.y / imageHeight) * 100;
-                    startTime = process.hrtime();
                     unirest.post(`https://${process.env.ENDPOINT_DOMAIN}/api/v1/images/${imageId}/einstein_box`).headers({
                         'Content-Type': 'application/json',
                         'Authorization': `Token token="${token}"`
@@ -88,13 +81,11 @@ app.post('/webhook', function (req, res) {
                         top: percentageY,
                         left: percentageX
                     }).end(function(response){
-                        console.log('End time', process.hrtime()[0]-startTime[0]);
                         if (imageAttributesPair[imageAttributesPair.length - 1] === attribute) {
                             let csvJson = jsonfile.readFileSync(filepath);
                             csvJson[externalId].webhookCompleted = true;
                             jsonfile.writeFileSync(filepath, csvJson);
                         }
-                        console.log('done');
                     });
                 }
             })(attribute)
@@ -111,14 +102,10 @@ app.post('/webhook', function (req, res) {
 app.post('/upload-csv', function(req, res) {
     if (!req.files)
         return res.status(400).send('No files were uploaded.');
-    
-    // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file 
     let csvFile = req.files.csvFile;
     let albumId = req.body.albumId;
-    
     let bufferStream = new Stream.PassThrough();
     bufferStream.end(csvFile.data);
-    // let csvJsonId = uuidV4();
     let utcdate = ("0" + date.getUTCDate()).slice(-2);
     let month =   ("0" + date.getMonth()).slice(-2);
     let csvJsonId = date.getFullYear()+'-'+month+'-'+utcdate+'-'+date.getHours()+date.getMinutes()+date.getSeconds();
@@ -157,5 +144,5 @@ app.use(function(req, res) {
 });
 
  app.listen(process.env.PORT, function () {
-    console.log(`Example app listening on port ${process.env.PORT}`);
+    
 });
